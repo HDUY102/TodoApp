a585679380ed8edf8caaba782527627a21449f4e
HDUY <leduy020702@gmail.com>
2023-11-25 17:03:39 +0700

    prisma

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,17 +1,28 @@
 import { tasks } from "@/app/lib/placeholder-data";
+import prisma from "@/prisma/client";
 import { NextRequest,NextResponse } from "next/server";
 
 // CRUD: 1. GET (R): lấy, POST(C): Tạo, DELETE(D): Xóa, PUT(U): cập nhật
 
 // get all tasks
 export async function GET(request:NextRequest) {
-    return NextResponse.json(tasks);
+    const task = await prisma.task.findMany()
+    return NextResponse.json(task);
 }
 
 // add new task to tasks list
 export async function POST(request:NextRequest) {
     const body = await request.json();
     //add new task in body
-    tasks.push(body);
-    return NextResponse.json(body);
+    // tasks.push(body);
+
+    //add new task to mysql db
+    const task = await prisma.task.create({
+        data: {
+          taskName: body.taskName,
+          isDone: false,
+          isDeleted: false
+        },
+    })
+    return NextResponse.json(task);
 }
